32da20440cb79e896d85a36e38b426d6b5dc3fce
Digi1312 <107186397+Digi1312@users.noreply.github.com>
2025-03-12 02:43:14 +0530

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Connect to MongoDB Atlas
-client = MongoClient("mongodb+srv://digvijaysonawane007:ABCDE@cluster0.rpjgb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("mongodb+srv://digvijaysonawane007:hello@cluster0.rpjgb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["User_Data"]
 users_collection = db["Users"]
 
